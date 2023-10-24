c5dbd6ced1ecfc4cbf2762e9347a925d774abfed
Rasheek <sallanrasheek@gmail.com>
2023-10-24 12:58:28 +0530

    lorem ipsum

--- Slider/src/App.jsx
+++ Slider/src/App.jsx
@@ -3,7 +3,7 @@ import SlickCarousel from "./SlickCarousel";
 const App = () => {
   return (
     <main>
-      <h1 style={{fontFamily:'-moz-initial', textAlign:'center', margin:'30px',color:'purple',fontWeight:'bold'}}>Web Development Team</h1>
+      {/* <h1 style={{fontFamily:'-moz-initial', textAlign:'center', margin:'30px',color:'purple',fontWeight:'bold'}}>Web Development Team</h1> */}
       <SlickCarousel/>
     </main>
   );
