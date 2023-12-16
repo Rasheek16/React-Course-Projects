662d49638f92e2940b3c5a12707d1b7e79378209
Rasheek <sallanrasheek@gmail.com>
2023-12-16 23:54:05 +0530

    uploaded

---
+++ Navbar/src/Navbar.jsx
@@ -0,0 +1,59 @@
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+import { useState, useRef } from "react";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    console.log();
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : `0px`,
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* SOCIAL LINKS */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
